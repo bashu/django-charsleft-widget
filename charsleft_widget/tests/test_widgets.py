508dedf25ce6c1de585677840dc872dd154cddb2
Basil Shubin <basil.shubin@gmail.com>
2013-08-05 11:45:15 +0700

    test jinja template

--- charsleft_widget/tests/test_widgets.py
+++ charsleft_widget/tests/test_widgets.py
@@ -9,6 +9,26 @@ from ..widgets import CharsLeftArea
 
 class CharsLeftAreaTest(TestCase):
 
+    def test_jinja_template(self):
+        old_USE_JINJA = getattr(settings, 'USE_JINJA', False)
+        settings.USE_JINJA = True
+
+        self.field = forms.CharField(required=False, widget=CharsLeftArea)
+        response = self.field.widget.render('value', 'test', {'id': 'id_field', 'maxlength': 512})
+        self.assertTrue('charsleft' in response and '508' in response)
+
+        settings.USE_JINJA = old_USE_JINJA
+
+    def test_django_template(self):
+        old_USE_JINJA = getattr(settings, 'USE_JINJA', False)
+        settings.USE_JINJA = False
+
+        self.field = forms.CharField(required=False, widget=CharsLeftArea)
+        response = self.field.widget.render('value', 'test', {'id': 'id_field', 'maxlength': 512})
+        self.assertTrue('charsleft' in response and '508' in response)
+
+        settings.USE_JINJA = old_USE_JINJA
+
     def test_default(self):
         self.field = forms.CharField(required=False, widget=CharsLeftArea)
 
