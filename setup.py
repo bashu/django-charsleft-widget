6bfd4396b0ec4fc637554ff72bdf9fbdb348a11e
Basil Shubin <basil.shubin@gmail.com>
2014-02-06 12:37:03 +0700

    bump version number

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-charsleft-widget',
-    version='0.1.2',
+    version='0.1.3',
     packages=['charsleft_widget'],
     include_package_data=True,
     license='BSD License',
