74073174c7ab13f1e456aa005ec902cb6e0b3d5c
Basil Shubin <basil.shubin@gmail.com>
2014-02-06 12:22:40 +0700

    improve widget

--- charsleft_widget/static/charsleft_widget/js/charsleft.js
+++ charsleft_widget/static/charsleft_widget/js/charsleft.js
@@ -5,3 +5,28 @@ var charsleft = function(source, maxlength) {
     }
     return maxlength - source.val().length;
 };
+
+(function ($) {
+    "use strict";
+    function add_charsleft_handlers() {
+        $('div.charsleft').each(function () {
+            var field = this.getAttribute('data-charsleft-field');
+            var maxlength = this.getAttribute('data-maxlength');
+
+            var $charsleftfield = $('textarea[name="' + field + '"]', this);
+
+            $charsleftfield.keyup(function (e, data) {
+                $(this).parent().find('.current').text(
+                    charsleft($(this), maxlength));
+            });
+
+            $charsleftfield.change(function (e, data) {
+                $(this).parent().find('.current').text(
+                    charsleft($(this), maxlength));
+            });
+        });
+    }
+    $(function () {
+        add_charsleft_handlers();
+    });
+}(jQuery || django.jQuery));
