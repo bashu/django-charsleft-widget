ad31a3e7e0be41d185e908a7a6f7275373cf82a0
Basil Shubin <basil.shubin@gmail.com>
2015-09-24 08:15:55 +0500

    Update fields.py

--- charsleft_widget/fields.py
+++ charsleft_widget/fields.py
@@ -5,8 +5,8 @@ from django import forms
 
 class CharField(forms.CharField):
 
-    def widget_attrs(self, widget):
-        attrs = super(CharField, self).widget_attrs(widget)  # pylint: disable=E1002
+    def widget_attrs(self, widget):  # pylint: disable=E1002
+        attrs = super(CharField, self).widget_attrs(widget)
         if self.max_length is not None:
             # The HTML attribute is maxlength, not max_length.
             attrs.update({'maxlength': str(self.max_length)})
