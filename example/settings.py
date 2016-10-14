9e483ed8b59067b31ab5e8803f21b879ac82c0b8
bashu <basil.shubin@gmail.com>
2016-10-14 13:12:53 +0700

    updated to django 1.10.x

--- example/settings.py
+++ example/settings.py
@@ -31,8 +31,28 @@ TEMPLATE_DIRS = (
     os.path.join(os.path.dirname(__file__), 'templates'),
 )
 
-# Application definition
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            os.path.join(os.path.dirname(__file__), 'templates'),
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
 
+# Application definition
 
 PROJECT_APPS = [
     'charsleft_widget',
--- runtests.py
+++ runtests.py
@@ -14,6 +14,25 @@ if not settings.configured:
     settings.configure(
         DEBUG = False,
         TEMPLATE_DEBUG = True,
+        TEMPLATES = [
+            {
+                'BACKEND': 'django.template.backends.django.DjangoTemplates',
+                'DIRS': [
+                ],
+                'APP_DIRS': True,
+                'OPTIONS': {
+                    'context_processors': [
+                        'django.contrib.auth.context_processors.auth',
+                        'django.template.context_processors.debug',
+                        'django.template.context_processors.i18n',
+                        'django.template.context_processors.media',
+                        'django.template.context_processors.static',
+                        'django.template.context_processors.tz',
+                        'django.contrib.messages.context_processors.messages',
+                    ],
+                },
+            },
+        ],
         DATABASES = {
             'default': {
                 'ENGINE': 'django.db.backends.sqlite3',
